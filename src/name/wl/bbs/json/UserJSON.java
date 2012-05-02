24ed6a25b2039b60ab51c6b1a09fc2bb51595f95
wanglun <iseuer@gmail.com>
2012-05-02 22:26:09 +0800

    User JSON

---
+++ src/name/wl/bbs/json/UserJSON.java
@@ -0,0 +1,52 @@
+package name.wl.bbs.json;
+
+import java.util.Hashtable;
+import java.util.Vector;
+import org.json.me.JSONArray;
+import org.json.me.JSONObject;
+
+import name.wl.bbs.http.HTTPRequestThread;
+import name.wl.bbs.json.ApiJSON;
+import name.wl.bbs.util.*;
+import name.wl.bbs.hjlp.*;
+
+public class UserJSON extends ApiJSON
+{
+    /* "/user/:id.json" */
+    private static String API = "/user/";
+
+    private String id;
+
+    private User user;
+
+    public UserJSON(String id)
+    {
+        this.id = id;
+    }
+
+    public void load()
+    {
+        HTTPRequestThread requestThread = new HTTPRequestThread(getURL(API + this.id + ".json"));
+        Event.observe(requestThread, "LOADED", this.requestListener);
+        requestThread.start();
+    }
+
+    public void loadContent(final String json)
+    {
+        Logger.debug("loadContent");
+        super.loadContent(json);
+
+        if (this.success) {
+            try {
+                /* parse the json */
+                // this.user
+            } catch (Exception e) {
+                Logger.debug("parse error");
+                this.success = false;
+            }
+        }
+
+        Event.trigger(this, "LOADED");
+    }
+}
+
