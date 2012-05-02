e4fa46e9338bd976e5ebff2dd94b89d61969d327
wanglun <iseuer@gmail.com>
2012-05-02 22:22:17 +0800

    MailDelete JSON

---
+++ src/name/wl/bbs/json/MailDeleteJSON.java
@@ -0,0 +1,57 @@
+package name.wl.bbs.json;
+
+import java.util.Hashtable;
+import org.json.me.JSONArray;
+import org.json.me.JSONObject;
+
+import name.wl.bbs.http.HTTPRequestThread;
+import name.wl.bbs.json.ApiJSON;
+import name.wl.bbs.util.*;
+import name.wl.bbs.hjlp.*;
+
+public class MailDeleteJSON extends ApiJSON
+{
+    private static String API = "/mail/delete.json";
+
+    /* MailboxJSON.INBOX etc */
+    private int type;
+
+    private int id;
+
+    /* ·µ»Ø */
+    private int result;
+
+    public MailDeleteJSON(int type, int id)
+    {
+        this.type = type;
+        this.id = id;
+    }
+
+    public void load()
+    {
+        Hashtable params = new Hashtable();
+        params.put("type", Integer.toString(this.type));
+        params.put("id", Integer.toString(this.id));
+
+        HTTPRequestThread requestThread = new HTTPRequestThread(getURL(API, AUTH), params);
+        Event.observe(requestThread, "LOADED", this.requestListener);
+        requestThread.start();
+    }
+
+    public void loadContent(final String json)
+    {
+        Logger.debug("loadContent");
+        super.loadContent(json);
+
+        if (this.success) {
+            try {
+                this.result = this.data.getInt("result");
+            } catch (Exception e) {
+                Logger.debug("token parse error");
+                this.success = false;
+            }
+        }
+
+        Event.trigger(this, "LOADED");
+    }
+}
