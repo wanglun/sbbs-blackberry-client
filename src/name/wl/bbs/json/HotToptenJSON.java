e7f13ccf95c1912f0df62ba03af84acc36c48907
wanglun <iseuer@gmail.com>
2012-05-02 22:41:51 +0800

    HotTopten JSON

---
+++ src/name/wl/bbs/json/HotToptenJSON.java
@@ -0,0 +1,48 @@
+package name.wl.bbs.json;
+
+import java.util.Hashtable;
+import java.util.Vector;
+import org.json.me.JSONArray;
+import org.json.me.JSONObject;
+
+import name.wl.bbs.http.HTTPRequestThread;
+import name.wl.bbs.json.ApiJSON;
+import name.wl.bbs.util.*;
+import name.wl.bbs.hjlp.*;
+
+public class HotToptenJSON extends ApiJSON
+{
+    private static String API = "/hot/topten.json";
+
+    /* ·µ»Ø */
+    private Vector topics;
+
+    public HotToptenJSON()
+    {
+    }
+
+    public void load()
+    {
+        HTTPRequestThread requestThread = new HTTPRequestThread(getURL(API));
+        Event.observe(requestThread, "LOADED", this.requestListener);
+        requestThread.start();
+    }
+
+    public void loadContent(final String json)
+    {
+        Logger.debug("loadContent");
+        super.loadContent(json);
+
+        if (this.success) {
+            try {
+                // this.topics
+                /* parse the json */
+            } catch (Exception e) {
+                Logger.debug("parse error");
+                this.success = false;
+            }
+        }
+
+        Event.trigger(this, "LOADED");
+    }
+}
