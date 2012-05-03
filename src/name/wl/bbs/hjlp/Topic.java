355036c18a70d097ebcc3ea49f73e4c58220164d
wanglun <iseuer@gmail.com>
2012-05-03 10:43:22 +0800

    use JSON String to construct Topic

--- src/name/wl/bbs/hjlp/Topic.java
+++ src/name/wl/bbs/hjlp/Topic.java
@@ -1,5 +1,8 @@
 package name.wl.bbs.hjlp;
 
+import org.json.me.*;
+import java.util.*;
+
 public class Topic
 {
     /* 帖子ID */
@@ -39,7 +42,7 @@ public class Topic
     private String author;
     
     /* 发表时间戳 */
-    private int time;
+    private long time;
 
     /* 标题 */
     private String title;
@@ -57,10 +60,47 @@ public class Topic
     private String last_author;
 
     /* 最后回复时间 */
-    private int last_time;
+    private long last_time;
 
     public Topic()
     {
+    }
+
+    public Topic(String json) throws JSONException
+    {
+        JSONObject data = new JSONObject(json);
+
+        this.id = data.getInt("id");
+        this.gid = data.getInt("gid");
+        this.board = data.getString("board");
+        this.size = data.getInt("size");
+        this.read = data.getInt("read");
+        this.replies = data.getInt("replies");
+        this.reid = data.getInt("reid");
+        this.unread = data.getBoolean("unread");
+        this.top = data.getBoolean("top");
+        this.mark = data.getBoolean("mark");
+        this.norep = data.getBoolean("norep");
+        this.author = data.getString("author");
+        this.time = data.getLong("time");
+        this.title = data.getString("title");
+        this.content = data.getString("content");
+        this.quote = data.getString("quote");
+        this.quoter = data.getString("quoter");
+        this.last_author = data.getString("last_author");
+        this.last_time = data.getLong("last_time");
+    }
+
+    public static Vector Topics(String json) throws JSONException
+    {
+        Vector topics = new Vector();
+        JSONArray arr = new JSONArray(json);
+
+        for (int i = 0; i < arr.length(); i++) {
+            topics.addElement(new Topic(arr.getString(i)));
+        }
+
+        return topics;
     }
 
     public int getId()
@@ -123,7 +163,7 @@ public class Topic
         return author;
     }
 
-    public int getTime()
+    public long getTime()
     {
         return time;
     }
@@ -153,7 +193,7 @@ public class Topic
         return last_author;
     }
 
-    public int getLastTime()
+    public long getLastTime()
     {
         return last_time;
     }
