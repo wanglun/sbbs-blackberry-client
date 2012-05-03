b32848a1e21247da0fd021f790178d7544ace679
wanglun <iseuer@gmail.com>
2012-05-03 11:23:53 +0800

    generate Board instance from JSON

--- src/name/wl/bbs/hjlp/Board.java
+++ src/name/wl/bbs/hjlp/Board.java
@@ -1,6 +1,7 @@
 package name.wl.bbs.hjlp;
 
 import java.util.*;
+import org.json.me.*;
 
 public class Board
 {
@@ -29,6 +30,43 @@ public class Board
 
     public Board()
     {
+    }
+
+    public Board(String name)
+    {
+        this.name = name;
+    }
+
+    public static Board BoardJSON(String json) throws JSONException
+    {
+        Board board = new Board();
+        JSONObject data = new JSONObject(json);
+
+        board.setName(data.getString("name"));
+        board.setDescription(data.getString("description"));
+        board.setCount(data.getInt("count"));
+        /* FIXME */
+        //this.bm = data.getString("bm");
+
+        board.setLeaf(data.getBoolean("leaf"));
+
+        if (!board.isLeaf()) {
+            board.setBoards(BoardsJSON(data.getString("boards")));
+        }
+
+        return board;
+    }
+
+    public static Vector BoardsJSON(String json) throws JSONException
+    {
+        Vector boards = new Vector();
+        JSONArray arr = new JSONArray(json);
+
+        for (int i = 0; i < arr.length(); i++) {
+            boards.addElement(BoardJSON(arr.getString(i)));
+        }
+
+        return boards;
     }
 
     public String getName()
@@ -64,5 +102,40 @@ public class Board
     public boolean isLeaf()
     {
         return leaf;
+    }
+
+    private void setName(String name)
+    {
+        this.name = name;
+    }
+
+    private void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    private void setCount(int count)
+    {
+        this.count = count;
+    }
+
+    private void setUsers(int users)
+    {
+        this.users = users;
+    }
+
+    private void setBm(Vector bm)
+    {
+        this.bm = bm;
+    }
+
+    private void setBoards(Vector boards)
+    {
+        this.boards = boards;
+    }
+
+    private void setLeaf(boolean leaf)
+    {
+        this.leaf = leaf;
     }
 }
