4774fc12fc99808f36db0391b69cf5a619621e80
wanglun <iseuer@gmail.com>
2012-05-07 10:28:13 +0800

    add Data store/load interface~

---
+++ src/name/wl/bbs/data/AuthData.java
@@ -0,0 +1,9 @@
+package name.wl.bbs.data;
+
+class AuthData extends Data {
+    private static final String NAME = "auth";
+
+    public AuthData()
+    {
+    }
+}
---
+++ src/name/wl/bbs/data/Data.java
@@ -0,0 +1,88 @@
+package name.wl.bbs.data;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import javax.microedition.rms.*;
+
+import name.wl.bbs.util.*;
+
+class Data
+{
+    protected static final String NAME = "";
+
+    public Data()
+    {
+    }
+
+    public void storeJSON(String json)
+    {
+        try {
+            RecordStore store = RecordStore.openRecordStore(NAME, true);
+            int count = store.getNumRecords();
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DataOutputStream os = new DataOutputStream(baos);
+
+            // write timestamp
+            os.writeUTF(json);
+
+            os.close();
+
+            byte[] data = baos.toByteArray();
+            if (count == 0) {
+                store.addRecord(data, 0, data.length);
+            } else {
+                store.setRecord(1, data, 0, data.length);
+            }
+            store.closeRecordStore();
+        } catch (Exception e) {
+            Logger.debug(e.toString());
+        }
+    }
+
+    public String loadJSON()
+    {
+        String json = "";
+
+        try {
+            RecordStore store = RecordStore.openRecordStore(NAME, true);
+            int count = store.getNumRecords();
+
+            if (count == 0)
+                return null;
+
+            byte[] data = store.getRecord(1);
+            DataInputStream is = new DataInputStream(new ByteArrayInputStream(data));
+
+            json = is.readUTF();
+
+            store.closeRecordStore();
+        } catch (Exception e) {
+            Logger.debug(e.toString());
+        }
+
+        return json;
+    }
+
+    public void destroy()
+    {
+        try {
+            RecordStore store = RecordStore.openRecordStore(NAME, true);
+            int count = store.getNumRecords();
+
+            if (count == 0)
+                return;
+            else {
+                store.deleteRecord(1);
+            }
+
+            store.closeRecordStore();
+        } catch (Exception e) {
+            Logger.debug(e.toString());
+        }
+    }
+}
