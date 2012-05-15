f0ffe40636eb2758c8dd801c9a71e297175ae1ec
wanglun <iseuer@gmail.com>
2012-05-15 11:08:14 +0800

    label & RichText field

---
+++ src/name/wl/bbs/ui/BbsLabelField.java
@@ -0,0 +1,11 @@
+package name.wl.bbs.ui;
+
+import net.rim.device.api.ui.component.*;
+
+public class BbsLabelField extends LabelField
+{
+    public BbsLabelField(String label)
+    {
+        super(label);
+    }
+}
---
+++ src/name/wl/bbs/ui/BbsRichTextField.java
@@ -0,0 +1,11 @@
+package name.wl.bbs.ui;
+
+import net.rim.device.api.ui.component.*;
+
+public class BbsRichTextField extends RichTextField
+{
+    public BbsRichTextField(String text)
+    {
+        super(text);
+    }
+}
